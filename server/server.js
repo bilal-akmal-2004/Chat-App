0cb0bcfc2109dc187fce1bb1c51ba41ed6f4cb7e
bilalakmal-2004 <ghulambeast468@gmail.com>
2025-07-11 04:18:59 +0500

    Some updation for the server.

--- server/server.js
+++ server/server.js
@@ -48,8 +48,13 @@ app.use("/api/messages", messageRouter);
 
 // connecteditn to mongdb
 await connectDb();
-const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
-  console.log("Server is runnig on port:", PORT);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 4000;
+  server.listen(PORT, () => {
+    console.log("Server is runnig on port:", PORT);
+  });
+}
+
+// export "server" for vercel
+export default server;
